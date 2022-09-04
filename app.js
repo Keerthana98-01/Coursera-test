7e70f77fdf39d5c4afe3c67a053be95dc6dadca5
Keerthana98-01 <keerthanamithu98@gmail.com>
2022-09-04 12:04:06 +0530

    module1 assignment

--- app.js
+++ app.js
@@ -1,5 +1,34 @@
 (function () {
   'use strict'
-  angular.module('myFirstApp',[]);
-  .controller('myFirstController',function(){});
+  angular.module('LunchCheck',[])
+  .controller('LunchCheckController',LunchCheckController);
+
+  function LunchCheckController($scope,$injector){
+    $scope.lunch_items="";
+    $scope.message="";
+    $scope.count_items=function (){
+    let count=0;
+
+if($scope.lunch_items==" " || ($scope.lunch_items=="")){
+  count=0;
+}else{
+    let items=$scope.lunch_items.split(",");
+    count=items.length;
+}
+
+      if($scope.message==" " || ($scope.message=="") || (count==0)){
+        $scope.message="Please enter data first";
+
+      }
+      else if(count>=1 && count<=3){
+        $scope.message="Enjoy!";
+
+      }
+      else if(count>3){
+        $scope.message="Too much!";
+
+      }
+    }
+
+  }
 })();
